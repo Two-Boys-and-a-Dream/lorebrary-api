ddc4532fb0aa72231fce971f17f5c6d93235c494
Raymond Cox <raymondc1911@gmail.com>
2023-06-06 12:00:04 -0400

    Boilerplate

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-require("dotenv").config()
-const express = require("express")
+require('dotenv').config()
+const express = require('express')
 const app = express()
+const cors = require('cors')
 const { PORT } = process.env
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
+// Configs
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World!')
 })
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+    console.log(`Example app listening on port ${PORT}`)
 })
