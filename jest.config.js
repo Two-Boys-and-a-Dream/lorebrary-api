8aa45a87421fc481c999ee6ada8667323aaf8a6d
Raymond Cox <raymondc1911@gmail.com>
2023-06-13 10:21:45 -0400

    Turned back on coverage

--- jest.config.js
+++ jest.config.js
@@ -17,10 +17,10 @@ module.exports = {
     clearMocks: true,
 
     // Indicates whether the coverage information should be collected while executing the test
-    collectCoverage: false,
+    collectCoverage: true,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    collectCoverageFrom: ['**/{!(index),}.js'],
+    collectCoverageFrom: ['Model/**/*.js', 'Routes/**/*.js'],
 
     // The directory where Jest should output its coverage files
     coverageDirectory: 'coverage',
@@ -29,9 +29,9 @@ module.exports = {
     coveragePathIgnorePatterns: [
         '\\\\node_modules\\\\',
         '\\\\coverage\\\\',
-        '\\\\data\\\\',
-        'jest.config.js',
-        'index.js',
+        // '\\\\data\\\\',
+        // 'jest.config.js',
+        // 'index.js',
     ],
 
     // Indicates which provider should be used to instrument code for coverage
